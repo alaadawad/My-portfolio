b8d685ddb87a1091c208020a2118fab75958ad08
ala'a dawad <alaad123h456k@gmail.com>
2024-10-28 14:28:02 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+let btnMenu = document.getElementById('btn-menu');
+let Menu = document.querySelector('.nav-links');
+
+btnMenu.onclick = function(){
+    btnMenu.classList.toggle('fa-times');
+    Menu.classList.toggle('active')
+}
+
+let header = document.querySelector('header');
+
+window.onscroll = function(){
+    if(this.scrollY >= 100){
+        header.classList.add('active')
+    }
+    else{
+        header.classList.remove('active')
+    }
+    btnMenu.classList.remove('fa-times')
+    Menu.classList.remove('active')
+}
+
+// 
+// document.querySelector('.btn').addEventListener('click', function(event) {
+//     event.preventDefault(); // منع السلوك الافتراضي
+//     alert("تم عرض مشروعك بنجاح!"); // رسالة منبثقة
+// });
